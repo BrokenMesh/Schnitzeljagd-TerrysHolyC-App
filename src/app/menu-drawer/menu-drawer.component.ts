51fb718d8307606105a5a9349c1f973bb585d326
Noah <Noah@DAS-LAPTOP>
2025-06-03 11:02:54 +0200

    changes

--- src/app/menu-drawer/menu-drawer.component.ts
+++ src/app/menu-drawer/menu-drawer.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonMenu, IonLabel, IonList, IonItem, IonButton } from '@ionic/angular/standalone';
 import { MenuController } from '@ionic/angular';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-menu-drawer',
@@ -9,10 +10,16 @@ import { MenuController } from '@ionic/angular';
   imports: [IonContent, IonList, IonMenu, IonItem, IonHeader, IonToolbar, IonTitle, IonLabel, IonButton]
 })
 export class MenuDrawerComponent {
-   @ViewChild(IonMenu) menu!: IonMenu;
+  @ViewChild(IonMenu) menu!: IonMenu;
+  private gameService = inject(GameService)
+
+  curMainscore: number = 0;
+  curBonusscore: number = 0;
 
   openMenu() {
     this.menu?.open();
+    this.curMainscore = this.gameService.state!.mainScore;
+    this.curBonusscore = this.gameService.state!.bonusScore;
   }
 
   closeMenu() {
